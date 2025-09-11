16c5cba28da922ecaac8d1fde64f43f3ddc37e21
osTIce <feella79@gmail.com>
2025-09-12 02:12:12 +0900

    feat: 기본 템플릿 추가 - resources/templates/user/login.html 생성 - LoginController에서 '/login' 요청 시 login.html 렌더링
    feat: 메인 페이지 레이아웃 추가
    - 큰 4개 버튼 추가 (새 설문 만들기, 내가 만든 설문, 응답 확인, 설정)
    - 기본 스타일링
    
    feat:  로그인 페이지 추가
    - 구현 진행중

--- src/main/java/com/yscode/easysurveymaking/easysurveymaking/controller/LoginController.java
+++ src/main/java/com/yscode/easysurveymaking/easysurveymaking/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @RequestMapping("/")
-    public String home(){
-        return "home";
+    @RequestMapping("/login")
+    public String login(){
+        return "user/login";
     }
 
 }
